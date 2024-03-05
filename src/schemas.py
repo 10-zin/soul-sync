157d8c22bb0986e54c590ffbb03461a98a85a389
10-zin <tenzinbhotia0@gmail.com>
2024-03-05 12:49:40 +0530

    cleaning code

--- src/schemas.py
+++ src/schemas.py
@@ -1,19 +1,3 @@
-# from sqlalchemy import Column, String, DateTime
-# from sqlalchemy.ext.declarative import declarative_base
-# from datetime import datetime
-
-# Base = declarative_base()
-
-# class ChatMessage(Base):
-#     __tablename__ = 'chat_messages'
-
-#     message_id = Column(String, primary_key=True, index=True)
-#     text_message = Column(String, index=True)
-#     from_id = Column(String, index=True)
-#     to_id = Column(String, index=True)
-#     time = Column(DateTime, default=datetime.utcnow)
-
-
 from pydantic import BaseModel
 from datetime import datetime
 
